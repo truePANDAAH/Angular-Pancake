a81bfefd26beaf92c446b316f81746b7c67d9695
D.D.D <pandaahftw@hotmail.com>
2022-03-12 18:55:08 +0100

    fix

--- Pancake/src/app/app.module.ts
+++ Pancake/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { DesingSystemComponent } from './pages/desing-system/desing-system.component';
 import { NavBarComponent } from './core/nav-bar/nav-bar.component';
